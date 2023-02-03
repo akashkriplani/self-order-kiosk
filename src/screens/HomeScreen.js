601b4b5f88d9765b15fcce235fc0b8ccf98a63de
Akash Kriplani <aakash23kriplani@gmail.com>
2023-02-04 00:21:20 +0530

    Configure material ui in application

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Box, Card, CardActionArea, Typography } from '@material-ui/core';
+import { TouchApp } from '@material-ui/icons';
+import Logo from '../components/Logo';
+import { useStyles } from '../styles';
+
+export default function HomeScreen() {
+  const styles = useStyles();
+
+  return (
+    <Card>
+      <CardActionArea>
+        <Box className={[styles.root, styles.red]}>
+          <Box className={[styles.main, styles.center]}>
+            <Typography component="h6" variant="h6">
+              Fast & Easy
+            </Typography>
+            <Typography component="h1" variant="h1">
+              Order <br /> & Pay <br /> here
+            </Typography>
+            <TouchApp fontSize="large"></TouchApp>
+          </Box>
+          <Box className={[styles.center, styles.green]}>
+            <Logo large></Logo>
+            <Typography component="h5" variant="h5">
+              Touch to start
+            </Typography>
+          </Box>
+        </Box>
+      </CardActionArea>
+    </Card>
+  );
+}
