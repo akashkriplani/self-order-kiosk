760948e168f2e43e1773556dcbe3ddef1a66c661
Akash Kriplani <aakash23kriplani@gmail.com>
2023-02-05 22:25:43 +0530

    Add a list of products to data.js file

--- data.js
+++ data.js
@@ -12,6 +12,71 @@ const data = {
       name: 'Burgers',
       image: '/images/burgers.jpg'
     }
+  ],
+  products: [
+    {
+      category: 'Beverages',
+      name: 'Coca-Cola',
+      calorie: 120,
+      price: 1,
+      image: '/images/t-mcdonalds-Coca-Cola-Classic-small.jpg'
+    },
+    {
+      category: 'Beverages',
+      name: 'Hot Chocolate',
+      calorie: 170,
+      price: 2,
+      image: '/images/t-mcdonalds-McCafe-Hot-Chocolate-Medium.jpg'
+    },
+    {
+      category: 'Beverages',
+      name: 'Vanilla Shake',
+      calorie: 360,
+      price: 1.5,
+      image: '/images/t-mcdonalds-Vanilla-McCafe-Shake-Medium.jpg'
+    },
+    {
+      category: 'Breakfast',
+      name: 'Bacon & Biscuit',
+      calorie: 90,
+      price: 1.9,
+      image: '/images/t-mcdonalds-Bacon-Egg-Cheese-Biscuit-Regular-Size-Biscuit.jpg'
+    },
+    {
+      category: 'Breakfast',
+      name: 'Blueberry Muffin',
+      calorie: 120,
+      price: 1.5,
+      image: '/images/t-blueberry-muffin.jpg'
+    },
+    {
+      category: 'Breakfast',
+      name: 'Big Breakfast',
+      calorie: 430,
+      price: 3,
+      image: '/images/s-mcdonalds-Big-Breakfast-Regular-Size-Biscuit.jpg'
+    },
+    {
+      category: 'Burgers',
+      name: 'Big Mac',
+      calorie: 200,
+      price: 1.9,
+      image: '/images/t-mcdonalds-Big-Mac.jpg'
+    },
+    {
+      category: 'Burgers',
+      name: 'Hamburger',
+      calorie: 410,
+      price: 1.5,
+      image: '/images/t-mcdonalds-Hamburger.jpg'
+    },
+    {
+      category: 'Burgers',
+      name: 'McDouble',
+      calorie: 320,
+      price: 3,
+      image: '/images/t-mcdonalds-McDouble.jpg'
+    }
   ]
 };
 
