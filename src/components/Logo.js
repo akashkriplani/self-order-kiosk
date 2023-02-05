d40fd5b6d16d8b05b00d42564b41619e154e168b
Akash Kriplani <aakash23kriplani@gmail.com>
2023-02-05 21:25:50 +0530

    Extend Logo component to accept both large size logo and regular size logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useStyles } from '../styles';
 
-export default function Logo() {
+export default function Logo({ large }) {
   const styles = useStyles();
 
-  return <img src="/images/logo.png" alt="Food order" className={styles.largeLogo} />;
+  return <img src="/images/logo.png" alt="Food order" className={large ? styles.largeLogo : styles.logo} />;
 }
