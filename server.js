412a7625375573710102a7bf592fb8b0566f7c7a
Akash Kriplani <aakash23kriplani@gmail.com>
2023-02-07 17:20:47 +0530

    Add BE API implementation for admin orders

--- server.js
+++ server.js
@@ -80,6 +80,31 @@ const Order = mongoose.model(
   )
 );
 
+// For admin user
+app.get('/api/orders', async (req, res) => {
+  const orders = await Order.find({ isDelivered: false, isCancelled: false });
+  res.send(orders);
+});
+
+app.put('/api/orders/:id', async (req, res) => {
+  const order = await Order.findById(req.params.id);
+  if (order) {
+    if (req.body.action === 'ready') {
+      order.isReady = true;
+      order.inProgress = false;
+    } else if (req.body.action === 'deliver') {
+      order.isDelivered = true;
+    } else if (req.body.action === 'cancel') {
+      order.isCancelled = true;
+    }
+    await order.save();
+
+    res.send({ message: 'Done' });
+  } else {
+    req.status(404).message({ message: 'Order not found' });
+  }
+});
+
 app.post('/api/orders', async (req, res) => {
   const lastOrder = await Order.find().sort({ number: -1 }).limit(1);
   const lastNumber = lastOrder.length === 0 ? 0 : lastOrder[0].number;
